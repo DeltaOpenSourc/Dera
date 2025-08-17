f4ba0a34f6a92f660c4feacf264643016e388151
DancerList <dislourens@gmail.com>
2025-08-18 06:02:02 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -146,5 +146,3 @@ async def index():
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)), log_level="info")
-
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)), log_level="info")
